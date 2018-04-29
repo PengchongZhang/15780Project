a801b04ba2c02026554c3b415bcf29006d8f7018
Jing_Xu <kayjade519@gmail.com>
2018-04-29 19:43:21 -0400

    add dir

--- ai_project/assign_task/views.py
+++ ai_project/assign_task/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .partition import Partition
 
 
 # Create your views here.
 def index(request):
     return HttpResponse('This is the index page.')
+
+
+def get_result(request):
+    return HttpResponse('get result for partition problem')
+
+
+def solve_partition(request):
+    if request.method == 'POST':
+        return HttpResponse('solve partition problem')
