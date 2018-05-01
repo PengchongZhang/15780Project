f6757d2862247c18a131c36e309e54420f6a78d1
Jing_Xu <kayjade519@gmail.com>
2018-05-01 14:02:52 -0400

    extract and valid input

--- ai_project/assign_task/static/js/assign.js
+++ ai_project/assign_task/static/js/assign.js
@@ -1,9 +1,21 @@
 var tasksNum;
 var devsNum;
+var inputForm;
+
+function checkInputNotNull (formData) {
+    for (var i=0; i<formData.length; i++) {
+        if (formData[i].value === "")
+            return false;
+    }
+    return true;
+}
 
 $(document).ready(function () {
+    inputForm = $('#task-form');
+
     $('#numbers-btn').unbind('click').on('click', function (e) {
         e.preventDefault();
+        var intro = $('#intro');
         tasksNum = $('#task-number').val();
         devsNum = $('#developer-number').val();
         if (tasksNum === "" || devsNum === "") {
@@ -12,28 +24,36 @@ $(document).ready(function () {
         $('#numbers').hide();
         var estDiv = $('#estimate-input');
         var voteDiv = $('#vote-input');
-        for (var i = 0; i < devsNum; i++) {
+        for (var i = 0; i < devsNum; i++) {  // append text input for estimation
             var estId = 'est-dev' + i;
             var estLabel = $('<label>').attr({for: estId}).text('estimation of developer' + (i + 1));
-            var estInput = $('<input>').attr({type: 'text', name: estId, id: estId, required: true});
+            var estInput = $('<input required>').attr({type: 'text', name: estId, id: estId});
             estDiv.append(estLabel);
             estDiv.append(estInput);
             estDiv.append($('<br>'));
         }
         estDiv.show();
-        for (var j = 0; j < devsNum; j++) {
+        for (var j = 0; j < devsNum; j++) {  // append text input for vote order
             var voteId = 'vote-dev' + j;
             var voteLabel = $('<label>').attr({for: voteId}).text('expert order of developer' + (j + 1));
-            var voteInput = $('<input>').attr({type: 'text', name: voteId, id: voteId, required: true});
+            var voteInput = $('<input required>').attr({type: 'text', name: voteId, id: voteId});
             voteDiv.append(voteLabel);
             voteDiv.append(voteInput);
             voteDiv.append($('<br>'));
         }
         voteDiv.show();
         $('#submit-btn').show();
+        intro.text("Esitame the work load of " + tasksNum + " tasks for " + devsNum + " developers.");
+        intro.show();
     });
 
-    $('#submit-btn').unbind('click').on('click', function (e) {
+    $('#submit-btn').unbind('click').on('click', function (e) {  // solve assign task problem
         e.preventDefault();
-    })
+        var formData = inputForm.serializeArray();
+        if (formData === undefined || !checkInputNotNull(formData))  return alert('Invalid input!');
+        var url = inputForm.attr("action");
+        $.post(url, formData).done(function (data) {
+            console.log(data);
+        });
+    });
 });
--- ai_project/assign_task/views.py
+++ ai_project/assign_task/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import QueryDict
 from .partition import Partition
 import json
 
@@ -10,9 +11,59 @@ def index(request):
     return render(request, 'index.html')
 
 
-# api for solving the task assignment problem
+def check_valid_est(est):
+    for i in range(len(est)):
+        if est[i] < 0:
+            return False
+    return True
+
+
+def check_valid_vote(vote):
+    index = set()
+    for i in range(len(vote)):
+        if vote[i] < 1 or vote[i] > len(vote) or vote[i] in index:
+            return False
+        index.add(vote[i])
+    return True
+
+
+def extract_input(post_data):
+    """
+    extract input from the post data
+    :param post_data:
+    :return:
+    """
+    try:
+        task_num = int(post_data['task-number'])
+        developer_num = int(post_data['developer-number'])
+        vote_order = []
+        estimate = []
+        for i in range(task_num):
+            est_key = 'est-dev{}'.format(i)
+            vote_key = 'vote-dev{}'.format(i)
+            dev_est_str = post_data[est_key].strip().replace(' ', '').split(',')
+            dev_vote_str = post_data[vote_key].strip().replace(' ', '').split(',')
+            if len(dev_vote_str) != developer_num or len(dev_est_str) != task_num:
+                return None
+            dev_est = [float(dev_est_str[j]) for j in range(task_num)]
+            if not check_valid_est(dev_est):
+                return None
+            estimate.append(dev_est)
+            dev_vote = [int(dev_vote_str[j]) for j in range(developer_num)]
+            if not check_valid_vote(dev_vote):
+                return None
+            vote_order.append(dev_vote)
+        return task_num, developer_num, estimate, vote_order
+    except ValueError:
+        return None
+
+
 def solve_assignment(request):
+    """
+    api for solving the task assignment problem
+    :param request:
+    :return:
+    """
     if request.method == 'POST':
-        data = json.loads(request.body)
-        print(int(data['data']['tasks'][0]['index']))
+        input_data = extract_input(request.POST)
         return HttpResponse('solve partition problem')
