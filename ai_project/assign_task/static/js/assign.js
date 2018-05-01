5ba77a77e74e7783f8884a3beb0e93122ea79704
Jing_Xu <kayjade519@gmail.com>
2018-05-01 16:24:56 -0400

    print result in front end

--- ai_project/assign_task/static/js/assign.js
+++ ai_project/assign_task/static/js/assign.js
@@ -1,13 +1,78 @@
 var tasksNum;
 var devsNum;
 var inputForm;
+var estDiv = $('#estimate-input');
+var voteDiv = $('#vote-input');
+var introEst = $('#intro-est');
+var introVote = $('#intro-vote');
+var weightInput = $('#weight-input');
 
-function checkInputNotNull (formData) {
-    for (var i=0; i<formData.length; i++) {
+/**
+ * check the input estimation and vote order are not empty
+ * @param formData
+ * @returns {boolean}
+ */
+function checkInputNotNull(formData) {
+    for (var i = 0; i < formData.length; i++) {
         if (formData[i].value === "")
             return false;
     }
     return true;
+}
+
+/**
+ * remove all text input and show the input for numbers again
+ */
+function cleanUpForm() {
+    estDiv.empty();
+    estDiv.hide();
+    introEst.hide();
+    voteDiv.empty();
+    voteDiv.hide();
+    introVote.hide();
+    $('#task-number').val("");
+    $('#developer-number').val("");
+    $('#numbers').show();
+    $('#expert-weight').val("");
+    weightInput.hide();
+}
+
+function showEstRes (estimate) {
+    for(var i=0; i<estimate.length; i++) {
+        var task_id = i+1;
+        var task_est = $('<li>').text('task ' + task_id + ": " + estimate[i]);
+        $('#res-est').append(task_est);
+    }
+}
+
+function showAssignRes (assignment) {
+    for(var i=0; i<assignment.length; i++) {
+        var dev_assign = $('<li>').text('developer ' + assignment[i]['dev_id']);
+        var dev_tasks = $('<ul>');
+        var tasks = assignment[i]['tasks'];
+        for(var j=0; j<tasks.length; j++) {
+            var task_assign = $('<li>').text('task: ' + tasks[j]['task_id'] + ' proportion: ' + tasks[j]['proportion']);
+            dev_tasks.append(task_assign);
+        }
+        dev_assign.append(dev_tasks);
+        $('#res-assign').append(dev_assign);
+    }
+}
+
+function handleSolveRes (data) {
+    console.log(data);
+    if(!data.valid)
+        return window.alert('Unable to assign task because your input is invalid!');
+    // basic information
+    $('#res-task-number').text(data['task_number']);
+    $('#res-dev-number').text(data['developer_number']);
+    $('#res-expert-weight').text(data['expert_weight']);
+    $('#res-expert').text(data['expert']);
+    showEstRes(data['estimate']);
+    showAssignRes(data['assignment']);
+    // show result
+    $('#res').show();
+    $('#task-form').hide();
 }
 
 $(document).ready(function () {
@@ -15,6 +80,10 @@ $(document).ready(function () {
 
     $('#numbers-btn').unbind('click').on('click', function (e) {
         e.preventDefault();
+        console.log(estDiv.children().length);
+        console.log(voteDiv.children().length);
+        if(estDiv.children().length !== 0 || voteDiv.children().length !== 0)
+            return;
         var intro = $('#intro');
         tasksNum = $('#task-number').val();
         devsNum = $('#developer-number').val();
@@ -22,8 +91,7 @@ $(document).ready(function () {
             return alert('Invalid input!');
         }
         $('#numbers').hide();
-        var estDiv = $('#estimate-input');
-        var voteDiv = $('#vote-input');
+
         for (var i = 0; i < devsNum; i++) {  // append text input for estimation
             var estId = 'est-dev' + i;
             var estLabel = $('<label>').attr({for: estId}).text('estimation of developer' + (i + 1));
@@ -33,6 +101,7 @@ $(document).ready(function () {
             estDiv.append($('<br>'));
         }
         estDiv.show();
+        introEst.show();
         for (var j = 0; j < devsNum; j++) {  // append text input for vote order
             var voteId = 'vote-dev' + j;
             var voteLabel = $('<label>').attr({for: voteId}).text('expert order of developer' + (j + 1));
@@ -42,18 +111,18 @@ $(document).ready(function () {
             voteDiv.append($('<br>'));
         }
         voteDiv.show();
+        introVote.show();
         $('#submit-btn').show();
         intro.text("Esitame the work load of " + tasksNum + " tasks for " + devsNum + " developers.");
         intro.show();
+        weightInput.show();
     });
 
     $('#submit-btn').unbind('click').on('click', function (e) {  // solve assign task problem
         e.preventDefault();
         var formData = inputForm.serializeArray();
-        if (formData === undefined || !checkInputNotNull(formData))  return alert('Invalid input!');
+        if (formData === undefined || !checkInputNotNull(formData)) return alert('Invalid input!');
         var url = inputForm.attr("action");
-        $.post(url, formData).done(function (data) {
-            console.log(data);
-        });
+        $.post(url, formData).done(handleSolveRes);
     });
 });
--- ai_project/assign_task/views.py
+++ ai_project/assign_task/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.http import QueryDict
+from django.template.loader import render_to_string
+from django.http import HttpResponse, JsonResponse
 from .partition import Partition
 import json
 
@@ -19,11 +19,11 @@ def check_valid_est(est):
 
 
 def check_valid_vote(vote):
-    index = set()
+    dev_set = set()
     for i in range(len(vote)):
-        if vote[i] < 1 or vote[i] > len(vote) or vote[i] in index:
+        if vote[i] < 0 or vote[i] >= len(vote) or vote[i] in dev_set:
             return False
-        index.add(vote[i])
+        dev_set.add(vote[i])
     return True
 
 
@@ -36,9 +36,10 @@ def extract_input(post_data):
     try:
         task_num = int(post_data['task-number'])
         developer_num = int(post_data['developer-number'])
+        expert_weight = float(post_data['expert-weight'])
         vote_order = []
         estimate = []
-        for i in range(task_num):
+        for i in range(developer_num):
             est_key = 'est-dev{}'.format(i)
             vote_key = 'vote-dev{}'.format(i)
             dev_est_str = post_data[est_key].strip().replace(' ', '').split(',')
@@ -49,13 +50,29 @@ def extract_input(post_data):
             if not check_valid_est(dev_est):
                 return None
             estimate.append(dev_est)
-            dev_vote = [int(dev_vote_str[j]) for j in range(developer_num)]
+            dev_vote = [int(dev_vote_str[j])-1 for j in range(developer_num)]
             if not check_valid_vote(dev_vote):
                 return None
             vote_order.append(dev_vote)
-        return task_num, developer_num, estimate, vote_order
+        return developer_num, task_num, expert_weight, estimate, vote_order
     except ValueError:
         return None
+
+
+def format_output(mix_est, dev_workload):
+    for i in range(len(mix_est)):
+        mix_est[i] = round(mix_est[i], 2)
+    assignment = []
+    for i in range(len(dev_workload)):
+        tasks = []
+        for j in range(len(dev_workload[i])):
+            if dev_workload[i][j] > 1e-12:
+                task = {'task_id': j+1, 'proportion': round(dev_workload[i][j], 2)}
+                tasks.append(task)
+        dev_assign = {'dev_id': i + 1, 'tasks': tasks}
+        assignment.append(dev_assign)
+    context = {'estimate': mix_est, 'assignment': assignment}
+    return context
 
 
 def solve_assignment(request):
@@ -66,4 +83,16 @@ def solve_assignment(request):
     """
     if request.method == 'POST':
         input_data = extract_input(request.POST)
-        return HttpResponse('solve partition problem')
+        if input_data is None:
+            return JsonResponse({'valid': False})
+        else:
+            solver = Partition()
+            expert, est, workload = solver.handler(input_data[0], input_data[1],
+                                                   input_data[2], input_data[3], input_data[4])
+            context = format_output(est, workload)
+            context['expert'] = expert + 1
+            context['valid'] = True
+            context['developer_number'] = input_data[0]
+            context['task_number'] = input_data[1]
+            context['expert_weight'] = input_data[2]
+            return JsonResponse(context)
